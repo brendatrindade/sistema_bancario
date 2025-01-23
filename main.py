da32741eb55192d1bbfe82dffa123c0e8fc9cb1f
brendatrindade <brendatrindde@gmail.com>
2025-01-23 15:48:32 -0300

    :sparkles: feat: add operacao de deposito

--- main.py
+++ main.py
@@ -15,3 +15,15 @@ limite = 500
 saques_efetuados = 0
 LIMITE_SAQUES_DIARIO = 3
 
+while (1):
+    selecao_usuario = int(input("Digite o numero da operacao que deseja realizar: "))
+    #deposito
+    if (selecao_usuario == 1):
+        valor_deposito = int(input("Digite o valor que deseja depositar: "))
+        saldo += valor_deposito
+        print("Voce depositou R$ %.2f.\n", valor_deposito)
+    # elif (selecao_usuario == 2):
+    # elif (selecao_usuario == 3):
+    # elif (selecao_usuario == 0):
+    # else:
+    
